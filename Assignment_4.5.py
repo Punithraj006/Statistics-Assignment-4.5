3b395fbd373af02313e21835f90d6ed18cc98f11
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 11:58:11 +0530

    Statistics Assignment 4.5

---
+++ Assignment_4.5.py
@@ -0,0 +1,18 @@
+Problem Statement: 4.5
+
+A die marked A to E is rolled 50 times. Find the probability of getting a “D” exactly 5 times.
+
+
+
+Solution:
+    
+n = 50,
+k = 5,
+n - k = 45.
+
+The probability of success = probability of getting a “D”
+=> s = 1/5
+
+Hence, the probability of failure = probability of not getting a “D”.
+
+=> 1 - s = 4/5.
